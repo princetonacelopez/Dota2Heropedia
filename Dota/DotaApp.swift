7f25df7468a556811b2d75c04602a3bf7f8d191e
Princeton Ace Lopez <princetonacelopez@icloud.com>
2021-02-01 15:51:58 +0800

    Initial Commit

---
+++ Dota/DotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotaApp.swift
+//  Dota
+//
+//  Created by Princeton Ace Lopez on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct DotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
